c30aef5b3c2be6cd74769d08655adb6f463279dc
CoolSpy3 <coolspythree@gmail.com>
2021-12-11 01:02:32 -0800

    update versions

--- src/main/java/com/coolspy3/csonlinefriends/CSOnlineFriends.java
+++ src/main/java/com/coolspy3/csonlinefriends/CSOnlineFriends.java
@@ -24,7 +24,7 @@ import net.hypixel.api.reply.FriendsReply.FriendShip;
 import net.hypixel.api.reply.StatusReply.Session;
 
 @Mod(id = "csonlinefriends", name = "CSOnlineFriends",
-        description = "Provides listing of currently online Hypixel friends.", version = "2.0.0",
+        description = "Provides listing of currently online Hypixel friends.", version = "2.0.1",
         dependencies = {"csmodloader:[1,2)", "cspackets:[1,2)", "csutils:[1,2)",
                 "cshypixelapi:[2,3)"})
 public class CSOnlineFriends implements Entrypoint
